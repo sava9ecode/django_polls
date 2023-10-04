7d2de8db85525e3b98a218a5bae37ab9e8d3509f
sava9ecode <sava9emode@yandex.ru>
2023-10-05 01:07:18 +0300

    Add tests

--- polls/models.py
+++ polls/models.py
@@ -13,10 +13,11 @@ class Question(models.Model):
         return self.question_text
 
     def get_absolute_url(self):
-        return reverse("polls:detail", args=[str(self.id)])
+        return reverse("polls:detail", args=(self.id,))
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
 class Choice(models.Model):
--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,8 @@
+from typing import Any
+from django.db import models
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 
@@ -12,17 +15,25 @@ class IndexView(generic.ListView):
     context_object_name = "latest_question_list"
 
     def get_queryset(self):
-        return Question.objects.order_by("-pub_date")[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by(
+            "-pub_date"
+        )[:5]
 
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = "polls/detail.html"
 
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
 
 class ResultsView(generic.DetailView):
     model = Question
     template_name = "polls/results.html"
+
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 def votes(request, question_id):
@@ -42,4 +53,4 @@ def votes(request, question_id):
     else:
         selected_choice.votes += 1
         selected_choice.save()
-        return HttpResponseRedirect(reverse("polls:results", args=[str(question.id)]))
+        return HttpResponseRedirect(reverse("polls:results", args=(question_id,)))
